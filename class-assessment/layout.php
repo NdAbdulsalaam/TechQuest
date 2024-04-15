2c060e07f62976828934c94e84bebef442ae568d
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-04-15 10:58:36 +0100

    set-up the layout

---
+++ class-assessment/layout.php
@@ -0,0 +1,6 @@
+<!-- bootstrap -->
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">  
+</head>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://kit.fontawesome.com/3df60fe6e2.js" crossorigin="anonymous"></script>
