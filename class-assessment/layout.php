f13bcd60c4dec1c4dcafffa0e08482e73e571b17
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-04-15 11:13:30 +0100

    change the fontawesome to link to my own

--- class-assessment/layout.php
+++ class-assessment/layout.php
@@ -1,6 +1,7 @@
 <!-- bootstrap -->
 <head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">  
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="class-assessment\assets\css\style.css" rel="stylesheet">
 </head>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://kit.fontawesome.com/3df60fe6e2.js" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/dd6e3edad7.js" crossorigin="anonymous"></script>
