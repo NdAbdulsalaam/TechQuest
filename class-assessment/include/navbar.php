9b1b40fdb749ab59c3229a20b02eb8e7b25b69ee
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-04-15 11:45:40 +0100

    write the login page

---
+++ class-assessment/include/navbar.php
@@ -0,0 +1,23 @@
+
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+      Bootstrap
+    </a>
+  </div>
+</nav>
+<ul class="nav justify-content-end">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+  </li>
+  </ul>
--- class-assessment/login.php
+++ class-assessment/login.php
@@ -1,1 +1,23 @@
-code rege
+<head>
+<?php require_once('layout.php') ?>
+</head>
+
+    <?php require_once('include/navbar.php') ?>
+
+<div>
+<form class="w-50 mb-3 mt-auto ms-auto me-auto">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <button type="submit" class="btn btn-primary">Login</button>
+</form>
+
+    <div>
+    <p>Don't have an account, <a href="register.php">Register<a></p>
+    </div>
+</div>
