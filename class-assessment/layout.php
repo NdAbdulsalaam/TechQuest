c0388224b601c532aef46032b970b882ef9a52d3
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-04-18 11:02:09 +0100

    remove duplicate files after the class assessment

--- class-assessment/layout.php
+++
@@ -1,8 +0,0 @@
-<!-- bootstrap -->
-<head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-<link href="class-assessment\assets\css\style.css" rel="stylesheet">
-</head>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://kit.fontawesome.com/dd6e3edad7.js" crossorigin="anonymous"></script>
-
