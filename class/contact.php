29536d760db8358aa02e8e0952b26004de9297aa
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-04-18 22:21:02 +0100

    fix bug in contact form

--- class/contact.php
+++ class/contact.php
@@ -1,57 +1,62 @@
 <?php
     session_start();
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require 'vendor/autoload.php';
 
 
-if(isset($_POST['sendemail'])){
+if(isset($_POST['sendmail'])){
     $name = $_POST['name'];
     $subject = $_POST['subject'];
     $body = $_POST['body'];
     $email = $_POST['email'];
+
+    // $my_mail = $_ENV['MAIL_USER'];
+    // $my_pass = $_ENV['MAIL_PASS'];
+
+
+
+    $mail = new PHPMailer(true);
+
+    try {
+
+        //Server settings
+        $mail->SMTPDebug = 0;                       //Enable verbose debug output SMTP::DEBUG_SERVER
+        $mail->isSMTP();                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';        //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                   //Enable SMTP authentication
+        $mail->Username   = "$my_mail";     //SMTP username
+        $mail->Password   = "$my_pass";     //SMTP password
+        $mail->SMTPSecure = 'tls';                  //Enable implicit TLS encryption
+        $mail->Port       = 587;                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+
+        //Recipients
+        $mail->setFrom("$my_mail", 'Nurudeen Mailer');
+        $mail->addAddress("$my_mail", 'Nurdudeen User');     //Add a recipient
+        //    $mail->addAddress('ellen@example.com');               //Name is optional
+        $mail->addReplyTo("$email");
+        // $mail->addCC('cc@example.com');
+        // $mail->addBCC('bcc@example.com');
+            
+        //Attachments
+        // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = "$subject";
+        $mail->Body    = "From:  $name <br> $body";
+        // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+        echo "<script>window.alert('Mail has been sent successfully!'</script>";
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
-
-$mail = new PHPMailer(true);
-
-try {
-
-    //Server settings
-    $mail->SMTPDebug = 2;                       //Enable verbose debug output SMTP::DEBUG_SERVER
-    $mail->isSMTP();                            //Send using SMTP
-    $mail->Host       = 'smtp.gfg.com;';        //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                   //Enable SMTP authentication
-    $mail->Username   = $_ENV['MAIL_USER'];     //SMTP username
-    $mail->Password   = $_ENV['MAIL_PASS'];     //SMTP password
-    $mail->SMTPSecure = 'tls';                  //Enable implicit TLS encryption
-    $mail->Port       = 587;                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-
-   //Recipients
-   $mail->setFrom($_ENV['MAIL_USER'], 'Nurudeen Mailer');
-   $mail->addAddress('olaitansalaam@outlook.com', 'Nurdudeen User');     //Add a recipient
-//    $mail->addAddress('ellen@example.com');               //Name is optional
-   $mail->addReplyTo($email, 'Feedback/Response');
-   $mail->addCC('cc@example.com');
-   $mail->addBCC('bcc@example.com');
-	
-    //Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = '$subject';
-    $mail->Body    = 'From: ' .$name .'<br>' .$body;
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-	$mail->send();
-	echo "Mail has been sent successfully!";
-} catch (Exception $e) {
-	echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
-
 ?>
 
 
@@ -67,7 +72,7 @@ try {
 <body>
     <?php include('include/navbar.php') ?>
     <div class="container my-5">
-        <form method="post" action="operator.php" class="mt-auto me-auto ms-auto w-50">
+        <form method="post" class="mt-auto me-auto ms-auto w-50">
             <div class="mb-3">
                 <label class="form-label">Full Name</label>
                 <input type="text" class="form-control" name="name">
@@ -85,9 +90,9 @@ try {
                 <input type="email" class="form-control" name="email">
             </div>
 
-            <button class="btn btn-primary" name="sendmail">Submit</button>
-            <p>Already have an account? <a href="login.php">Login</a>
-            <p>Don't have an account? <a href="register.php">Register</a>
+            <button type="submit" class="btn btn-primary" name="sendmail">Submit</button>
+            <p>Already have an account? <a href="login.php">Login</a></p>
+            <p>Don't have an account? <a href="register.php">Register</a></p>
         </form>
     </div>
 </body>
