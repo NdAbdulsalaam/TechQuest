0a4f5d694f2f1896dc73c4869d0a44631d609596
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-04-19 08:03:21 +0100

    change preview link to contact

--- class/include/navbar.php
+++ class/include/navbar.php
@@ -16,7 +16,7 @@
           <a class="nav-link" href="login.php">Login</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="preview.php">Preview</a>
+          <a class="nav-link" href="contact.php">Contact</a>
         </li>
       </ul>
     </div>
