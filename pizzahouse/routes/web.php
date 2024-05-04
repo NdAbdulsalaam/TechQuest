42df05f19fccae4935879939b5de265d27addf8a
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-05-04 07:51:04 +0100

    add pizzas route:

--- pizzahouse/routes/web.php
+++ pizzahouse/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pizzas', function() {
+    // return view('pizzas');
+    return ['name' => 'Abdulsalaam', 'gender' => 'male'];
+});
