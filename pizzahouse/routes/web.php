f3da8417bb9020c4a7ba59f13c25926a51ae0146
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-05-14 03:49:45 +0100

    protect urls

--- pizzahouse/routes/web.php
+++ pizzahouse/routes/web.php
@@ -6,12 +6,14 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 use App\Http\Controllers\Seller\DashboardController as SellerDashboardController;
 
-Route::get('/', function() {
+use App\Http\Middleware\Role; // Import the Role middleware
+
+Route::get('/', function () {
     return view('welcome');
 });
 
-
 Auth::routes();
+
 Route::get('/pizzas', [PizzaController::class, 'index'])->middleware('auth');
 Route::get('/pizzas/create', [PizzaController::class, 'create']);
 Route::post('/pizzas', [PizzaController::class, 'store']);
@@ -19,5 +21,5 @@ Route::get('/pizzas/{id}', [PizzaController::class, 'show'])->middleware('auth')
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('/admin-dashboard', [AdminDashboardController::class, 'index']);
-Route::get('/seller-dashboard', [SellerDashboardController::class, 'index']);
+Route::get('/admin-dashboard', [AdminDashboardController::class, 'index'])->middleware(Role::class . ':admin'); // Apply Role middleware
+Route::get('/seller-dashboard', [SellerDashboardController::class, 'index'])->middleware(Role::class . ':seller'); // Apply Role middleware
