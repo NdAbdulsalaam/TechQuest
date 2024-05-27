62639b5b562e1f9c02b36cfac9d7c300d9412fd5
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-05-27 10:32:28 +0100

    modify admin dashboard

--- TechQuestProject/resources/views/admin/dashboard.blade.php
+++ TechQuestProject/resources/views/admin/dashboard.blade.php
@@ -279,25 +279,11 @@
                 <div
                     class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                     <h5 class="m-0 font-weight-bold text-primary">Registered Staff Roles</h5>
-                    <div class="dropdown no-arrow">
-                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                            aria-labelledby="dropdownMenuLink">
-                            <div class="dropdown-header">Dropdown Header:</div>
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </div>
                 </div>
 
 
                 <div class="card-body">
-                    <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+                    <div id="chartContainer" style="height: 370px; width: 100%;">test</div>
 
                 </div>
             </div>
