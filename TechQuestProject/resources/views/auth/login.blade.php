5ff35875a6700f6d0a6fb674b722fbd1b50f7a61
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-05-27 08:51:12 +0100

    fix login/register image

--- TechQuestProject/resources/views/auth/login.blade.php
+++ TechQuestProject/resources/views/auth/login.blade.php
@@ -13,7 +13,9 @@
             <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
                 <div class="row">
-                    <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                    <div class="col-lg-6 d-none d-lg-block bg-login-image">
+                        <img src="{{ asset('dashboard/img/bg-TechQuest.jpg') }}">
+                    </div>
                     <div class="col-lg-6">
                         <div class="p-5">
                             <div class="text-center">
--- TechQuestProject/resources/views/auth/register.blade.php
+++ TechQuestProject/resources/views/auth/register.blade.php
@@ -8,7 +8,9 @@
     <div class="card-body p-0">
         <!-- Nested Row within Card Body -->
         <div class="row">
-            <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+            <div class="col-lg-5 d-none d-lg-block bg-register-image">
+                <img src="{{ asset('dashboard/img/bg-TechQuest.jpg') }}" width="99.5%">
+            </div>
             <div class="col-lg-7">
                 <div class="p-5">
                     <div class="text-center">
